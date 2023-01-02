685a39256a00d47fa42cddb9434ad3f4772f036f
Fat7y99 <fathy.nabil2022@gmail.com>
2023-01-02 15:01:21 +0200

    temp commit

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import {StyleSheet, StatusBar, View} from 'react-native';
 import LoginPage from './src/pages/LoginPage';
 import {Provider} from 'react-redux';
 import {Store} from './src/redux/Store';
-import {loginU} from './src/config/firebase';
+import {signUp, addCarsData} from './src/config/firebase';
 const App = () => {
-  loginU('fathy@gmail.com', 123698745);
+  // signUp('fathyy@gamail.com', 123698745);
+  addCarsData();
   return (
     <Provider store={Store}>
       <View style={styles.entireContaier}>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,10 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from 'firebase/app';
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'; //v9
+import 'firebase/compat/firestore'; //v9
+import 'firebase/storage';
 
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  doc,
-} from 'firebase/firestore';
+import {getFirestore} from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,79 +17,89 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-// export const addCarsData = async () => {
-//   console.log('db');
+app = firebase.initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const signUp = async (email, password) => {
+  await firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log('Added');
+    });
+};
 
-//   const Cars = [
-//     {
-//       carName: 'Mini Cooper',
-//       passenger: 4,
-//       pricePerDay: 200,
-//       type: 'Manual',
-//       imageUrl:
-//         'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-1.svg?alt=media&token=3b665cc8-8279-4b44-93de-e08a22284649',
-//       isLiked: 0,
-//     },
+export const addCarsData = async () => {
+  console.log('db');
+  const Cars = [
+    {
+      carName: 'Mini Cooper',
+      passenger: 4,
+      pricePerDay: 200,
+      type: 'Manual',
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-1.svg?alt=media&token=3b665cc8-8279-4b44-93de-e08a22284649',
+      isLiked: 0,
+    },
 
-//     {
-//       carName: 'Porshe 718 Cayman S',
-//       passenger: 6,
-//       pricePerDay: 300,
-//       type: 'Automatic',
-//       imageUrl:
-//         'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-2.svg?alt=media&token=6dbcf5f4-38c4-41af-9783-e5cec9c95147',
-//       isLiked: 0,
-//     },
-//     {
-//       carName: 'Porshe 718 Cayman S',
-//       passenger: 5,
-//       pricePerDay: 400,
-//       type: 'Manual',
-//       imageUrl:
-//         'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-3.svg?alt=media&token=07631824-860c-4d72-b29c-b9683ff5c694',
-//       isLiked: 0,
-//     },
-//     {
-//       carName: 'Porshe 718 Cayman S',
-//       passenger: 4,
-//       pricePerDay: 500,
-//       type: 'Automatic',
-//       imageUrl:
-//         'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-4.svg?alt=media&token=228195cd-ca1a-49e4-995e-fc5225464399',
-//       isLiked: 0,
-//     },
-//     {
-//       carName: 'Porshe 718 Cayman S',
-//       passenger: 6,
-//       pricePerDay: 550,
-//       type: 'Manual',
-//       imageUrl:
-//         'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-5.svg?alt=media&token=ef716f61-aaa8-41ee-a492-ad8bf8294ec9',
-//       isLiked: 0,
-//     },
-//   ];
-//   console.log('sayed');
+    {
+      carName: 'Porshe 718 Cayman S',
+      passenger: 6,
+      pricePerDay: 300,
+      type: 'Automatic',
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-2.svg?alt=media&token=6dbcf5f4-38c4-41af-9783-e5cec9c95147',
+      isLiked: 0,
+    },
+    {
+      carName: 'Porshe 718 Cayman S',
+      passenger: 5,
+      pricePerDay: 400,
+      type: 'Manual',
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-3.svg?alt=media&token=07631824-860c-4d72-b29c-b9683ff5c694',
+      isLiked: 0,
+    },
+    {
+      carName: 'Porshe 718 Cayman S',
+      passenger: 4,
+      pricePerDay: 500,
+      type: 'Automatic',
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-4.svg?alt=media&token=228195cd-ca1a-49e4-995e-fc5225464399',
+      isLiked: 0,
+    },
+    {
+      carName: 'Porshe 718 Cayman S',
+      passenger: 6,
+      pricePerDay: 550,
+      type: 'Manual',
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/car-dashboard-d6648.appspot.com/o/cars%2Fbooking-car-5.svg?alt=media&token=ef716f61-aaa8-41ee-a492-ad8bf8294ec9',
+      isLiked: 0,
+    },
+  ];
+  console.log('sayed');
 
-//   // try {
-//   //   const docRef = await addDoc(collection(db, 'users'), {
-//   //     first: 'Ada',
-//   //     last: 'Lovelace',
-//   //     born: 1815,
-//   //   });
-//   //   console.log('Document written with ID: ', docRef.id);
-//   // } catch (e) {
-//   //   console.error('Error adding document: ', e);
-//   // }
+  // try {
+  //   const docRef = await addDoc(collection(db, 'users'), {
+  //     first: 'Ada',
+  //     last: 'Lovelace',
+  //     born: 1815,
+  //   });
+  //   console.log('Document written with ID: ', docRef.id);
+  // } catch (e) {
+  //   console.error('Error adding document: ', e);
+  // }
 
-//   const carsCollection = collection(db, 'cars');
-//   Cars.map(async car => {
-//     // console.log(car);
-//     addDoc(carsCollection, car);
-//   });
-//   console.log('db');
-// };
+  const carsCollection = collection(db, 'cars');
+  Cars.map(async car => {
+    // console.log(car);
+    addDoc(carsCollection, car);
+  }).then(() => {
+    console.log('ADDEDDDDDDD');
+  });
+  console.log('db');
+};
 export const loginU = async (email, password) => {
   try {
     await firebase.auth().signInWithEmailAndPassword(email, password);
@@ -111,4 +116,4 @@ export const loginU = async (email, password) => {
 //   }
 // }
 
-export default app;
+// export default app;
