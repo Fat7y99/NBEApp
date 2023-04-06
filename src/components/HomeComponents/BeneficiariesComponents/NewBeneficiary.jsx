ff57d11eb4a2f17cf741ce2581b3b9ad67b2dc4f
Fathy Nabil Fathy El-Sayed <67173807+Fat7y99@users.noreply.github.com>
2023-04-06 22:54:09 +0200

    Some Modification For New Bene.

--- src/components/HomeComponents/BeneficiariesComponents/NewBeneficiary.jsx
+++ src/components/HomeComponents/BeneficiariesComponents/NewBeneficiary.jsx
@@ -14,148 +14,146 @@ import {
 } from '../../../services/firebase';
 import {setbeneficiaryData} from '../../../redux/beneficiary';
 import {setAppState} from '../../../redux/appState';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+
 const NewBeneficiary = ({navigation}) => {
   const dispatch = useDispatch();
   const beneficiary = useSelector(state => state.beneficiary);
 
   return (
-    <KeyboardAvoidingView
-      style={{flex: 1, backgroundColor: '#F0F2FA'}}
-      behavior="padding"
-      keyboardVerticalOffset={-150}>
-      <ScrollView style={{flex: 1}}>
-        <LogoHeader
-          navigation={navigation}
-          logoImage={Images.primaryLogo}
-          firstColor={Colors.primaryGreenColor}
-          image={Images.backButton}></LogoHeader>
-        <Pressable
-          onPress={async () => {
-            await uploadImage(dispatch);
-          }}>
-          {
-            <View
-              style={{
-                elevation: 3,
-                backgroundColor: 'white',
-                width: 138,
-                height: 138,
-                alignItems: 'center',
-                justifyContent: 'center',
-                borderRadius: 30,
-                alignSelf: 'center',
-                marginBottom: 25,
-              }}>
-              <Image
-                style={[
-                  {
-                    resizeMode: 'stretch',
-                  },
-                  beneficiary.imageUrl !== null
-                    ? {width: 138, height: 138, borderRadius: 30}
-                    : {},
-                ]}
-                source={
-                  beneficiary.imageUrl !== null
-                    ? {uri: beneficiary.imageUrl}
-                    : require('../../../../assets/images/ProfilePage/BeneficiariesImages/uploadImage.png')
-                }></Image>
-            </View>
+    <ScrollView style={{flex: 1}}>
+      <LogoHeader
+        navigation={navigation}
+        logoImage={Images.primaryLogo}
+        firstColor={Colors.primaryGreenColor}
+        image={Images.backButton}></LogoHeader>
+      <Pressable
+        onPress={async () => {
+          await uploadImage(dispatch);
+        }}>
+        {
+          <View
+            style={{
+              elevation: 3,
+              backgroundColor: 'white',
+              width: 138,
+              height: 138,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 30,
+              alignSelf: 'center',
+              marginBottom: 25,
+            }}>
+            <Image
+              style={[
+                {
+                  resizeMode: 'stretch',
+                },
+                beneficiary.imageUrl !== null
+                  ? {width: 138, height: 138, borderRadius: 30}
+                  : {},
+              ]}
+              source={
+                beneficiary.imageUrl !== null
+                  ? {uri: beneficiary.imageUrl}
+                  : require('../../../../assets/images/ProfilePage/BeneficiariesImages/uploadImage.png')
+              }></Image>
+          </View>
+        }
+      </Pressable>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+        }}>
+        <PrimaryInput
+          width="41%"
+          label="First name"
+          onChangeHandler={text =>
+            dispatch(setbeneficiaryData({first: 'firstName', last: text}))
           }
-        </Pressable>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-          }}>
-          <PrimaryInput
-            width="41%"
-            label="First name"
-            onChangeHandler={text =>
-              dispatch(setbeneficiaryData({first: 'firstName', last: text}))
-            }
-            isSecured={false}
-            elevation={3}></PrimaryInput>
+          isSecured={false}
+          elevation={3}></PrimaryInput>
 
-          <PrimaryInput
-            width="41%"
-            label="Last name"
-            onChangeHandler={text =>
-              dispatch(setbeneficiaryData({first: 'lastName', last: text}))
-            }
-            isSecured={false}
-            elevation={3}></PrimaryInput>
-        </View>
-        <View style={{alignItems: 'center'}}>
-          <PrimaryInput
-            width="90%"
-            label="Bank branch"
-            onChangeHandler={text =>
-              dispatch(setbeneficiaryData({first: 'bankBranch', last: text}))
-            }
-            isSecured={false}
-            elevation={3}></PrimaryInput>
-          <PrimaryInput
-            width="90%"
-            label="Account number"
-            onChangeHandler={text =>
-              dispatch(setbeneficiaryData({first: 'accountNumber', last: text}))
-            }
-            isSecured={false}
-            elevation={3}></PrimaryInput>
-          <PrimaryInput
-            width="90%"
-            label="Phone number"
-            onChangeHandler={text =>
-              dispatch(setbeneficiaryData({first: 'phoneNumber', last: text}))
-            }
-            isSecured={false}
-            elevation={3}></PrimaryInput>
-          <PrimaryInput
-            width="90%"
-            label="Email"
-            onChangeHandler={text => {
-              dispatch(setbeneficiaryData({first: 'email', last: text}));
-              console.log(beneficiary);
-            }}
-            isSecured={false}
-            elevation={3}></PrimaryInput>
-        </View>
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            //   backgroundColor: 'red',
-            marginBottom: 25,
-          }}>
-          <PrimaryButton
-            callBackFunction={async () => {
-              if (
-                !!beneficiary.firstName &&
-                !!beneficiary.lastName &&
-                !!beneficiary.bankBranch &&
-                !!beneficiary.email &&
-                !!beneficiary.phoneNumber &&
-                // beneficiary.imageUrl &&
-                !!beneficiary.accountNumber
-              ) {
-                dispatch(setAppState(true));
-                await addBeneficiary(beneficiary);
-                await getAccountsData(dispatch);
-                dispatch(setAppState(false));
-                console.log('Coming from new beneficiary');
-                navigation.goBack();
-              } else alert('invalid account data');
-            }}
-            height={50}
-            width={345}
-            backgroundColor={Colors.primaryGreenColor}
-            title="Add Beneficiar"
-            textColor="white"></PrimaryButton>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+        <PrimaryInput
+          width="41%"
+          label="Last name"
+          onChangeHandler={text =>
+            dispatch(setbeneficiaryData({first: 'lastName', last: text}))
+          }
+          isSecured={false}
+          elevation={3}></PrimaryInput>
+      </View>
+      <View style={{alignItems: 'center'}}>
+        <PrimaryInput
+          width="90%"
+          label="Bank branch"
+          onChangeHandler={text =>
+            dispatch(setbeneficiaryData({first: 'bankBranch', last: text}))
+          }
+          isSecured={false}
+          elevation={3}></PrimaryInput>
+        <PrimaryInput
+          width="90%"
+          label="Account number"
+          onChangeHandler={text =>
+            dispatch(setbeneficiaryData({first: 'accountNumber', last: text}))
+          }
+          isSecured={false}
+          elevation={3}></PrimaryInput>
+        <PrimaryInput
+          width="90%"
+          label="Phone number"
+          onChangeHandler={text =>
+            dispatch(setbeneficiaryData({first: 'phoneNumber', last: text}))
+          }
+          isSecured={false}
+          elevation={3}></PrimaryInput>
+        <PrimaryInput
+          width="90%"
+          label="Email"
+          onChangeHandler={text => {
+            dispatch(setbeneficiaryData({first: 'email', last: text}));
+            console.log(beneficiary);
+          }}
+          isSecured={false}
+          elevation={3}></PrimaryInput>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          //   backgroundColor: 'red',
+          marginBottom: 25,
+        }}>
+        <PrimaryButton
+          callBackFunction={async () => {
+            if (
+              !!beneficiary.firstName &&
+              !!beneficiary.lastName &&
+              !!beneficiary.bankBranch &&
+              !!beneficiary.email &&
+              !!beneficiary.phoneNumber &&
+              // beneficiary.imageUrl &&
+              !!beneficiary.accountNumber
+            ) {
+              dispatch(setAppState(true));
+              await addBeneficiary(beneficiary);
+              await getAccountsData(dispatch);
+              dispatch(setAppState(false));
+              console.log('Coming from new beneficiary');
+              navigation.goBack();
+            } else alert('invalid account data');
+          }}
+          height={50}
+          width={345}
+          backgroundColor={Colors.primaryGreenColor}
+          title="Add Beneficiar"
+          textColor="white"></PrimaryButton>
+      </View>
+      <KeyboardSpacer />
+    </ScrollView>
   );
 };
 export default NewBeneficiary;
